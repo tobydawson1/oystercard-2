232fdd386e8d1b26579b3eb0317fad33776e86e9
Liz Daly <liz7daly@gmail.com>
2020-01-27 15:53:48 +0000

    Add balance attribute

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+    it { is_expected.to respond_to(:balance)}
+
+    it 'returns balance of 0' do
+      expect(subject.balance).to eq 0
+    end
 end
