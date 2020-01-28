d80d97794363402e8aac297a8d2d0623a4d589c5
Liz Daly <liz7daly@gmail.com>
2020-01-28 15:23:21 +0000

    Add top-up limit - Step 6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
   attr_reader :balance
+  MAX_BALANCE = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Amount entered exceeds top limit of £#{MAX_BALANCE}" if maximum_limit?(amount)
     @balance += amount
+  end
+
+  private
+  def maximum_limit?(amount)
+    balance + amount > MAX_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,6 +11,13 @@ describe Oystercard do
     it 'tops up card balance' do
       expect{subject.top_up(10)}.to change{subject.balance}.by(10)
     end
+
+    it 'does not allow balance to exceed 90' do
+      max_balance = Oystercard::MAX_BALANCE
+      subject.top_up(max_balance)
+      expect{ subject.top_up(1) }.to raise_error("Amount entered exceeds top limit of £#{max_balance}")
+    end
+
   end
 
 end
