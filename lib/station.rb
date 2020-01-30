2dff1389c7789891a4828a23b12377c34dd38998
Toby Dawson <tobydawson1@gmail.com>
2020-01-30 14:11:54 +0000

    station class added

---
+++ lib/station.rb
@@ -0,0 +1,8 @@
+class Station
+
+    def initialize(name, zone)
+        @name = name
+        @zone = zone
+    end
+end
+
---
+++ spec/station_spec.rb
@@ -0,0 +1,10 @@
+require 'station'
+
+describe Station do
+
+    it { is_expected.to respond_to(:initialize).with(2).argument  }
+
+
+    it 'has a name' do
+    end
+end
